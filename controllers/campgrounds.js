1b61eb20e5e2f2fd9dcb5c0164ca1b0215fbbd9e
Mitchel Nelson <66894405+mitchelnelson@users.noreply.github.com>
2021-02-08 16:08:43 -0700

    seeded Canadian database

--- controllers/campgrounds.js
+++ controllers/campgrounds.js
@@ -20,6 +20,7 @@ module.exports.createCampground = async (req, res, next) => {
 			limit: 1
 		})
 		.send();
+	console.log(geoData.body.features[0]);
 	const campground = new Campground(req.body.campground);
 	campground.geometry = geoData.body.features[0].geometry;
 	campground.images = req.files.map(f => ({
--- seeds/index.js
+++ seeds/index.js
@@ -1,6 +1,5 @@
 const mongoose = require('mongoose');
-const cities = require('./canadacities');
-const { places, descriptors } = require('./seedHelpers');
+const camps = require('./CanadaCamp');
 const Campground = require('../models/campground');
 
 mongoose.connect('mongodb://localhost:27017/yelp-camp', {
@@ -18,29 +17,30 @@ db.once('open', () => {
 const sample = arr => arr[Math.floor(Math.random() * arr.length)];
 
 const seedDB = async () => {
-	await Campground.deleteMany({});
+	// await Campground.deleteMany({});
 	for (let i = 0; i < 200; i++) {
-		const random1738 = Math.floor(Math.random() * 1738);
+		const random1104 = Math.floor(Math.random() * 1104);
 		const price = Math.floor(Math.random() * 15) + 10;
 		const camp = new Campground({
 			author: '6016cc2e7fbdb3431766054e',
-			location: `${cities[random1738].city}, ${cities[random1738]
-				.province_name}`,
-			title: `${sample(descriptors)} ${sample(places)}`,
+			location: `${camps[random1104].location}, ${camps[random1104]
+				.province}`,
+			title: `${camps[random1104].campground}`,
 			geometry: {
 				type: 'Point',
-				coordinates: [cities[random1738].lng, cities[random1738].lat]
+				coordinates: [camps[random1104].lng, camps[random1104].lat]
 			},
 			images: [
 				{
 					url:
-						'https://res.cloudinary.com/dkdtpp6uy/image/upload/v1612398034/YelpCamp/tmatxkl704ee87ow99qh.jpg',
-					filename: 'YelpCamp/tmatxkl704ee87ow99qh'
+						'https://images.unsplash.com/photo-1478131143081-80f7f84ca84d?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1050&q=80',
+					filename: 'defaultPhoto'
 				}
 			],
-			description: `A lovely campground situated in the beautiful region of Canada`,
+			description: 'Campground',
 			price
 		});
+		// console.log(camp);
 		await camp.save();
 	}
 };
